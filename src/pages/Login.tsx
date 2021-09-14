801cc63b259f5fa6d8dc0aadc1396ac8594f7798
Vinicius Ferreira <viniciusferreirarosario5@gmail.com>
2021-09-14 13:02:40 -0300

    PJMSIDECHESTO

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,24 @@
-import React from "react";
-import { Container, FormRow, Button, Label, TextInput, Title, ButtonText } from "../styles/Login";
+import React from "react"; 
+import { Container, FormRow, Label, TextInput, Title, ButtonText } from "../styles/Login";
+import {Image, StyleSheet, Button} from 'react-native';
+import { useNavigation } from "@react-navigation/core";
 
-
+const styles = StyleSheet.create({
+    imagem: {
+        width: 60,
+        height: 60,
+    }
+  })
 
 export default function Login() {
+   
+    const navigation = useNavigation();
+    function handleComeco() {
+    navigation.navigate("Comeco");
+  }
     return (
         <Container>
+            <Image style = {styles.imagem}source={require('../img/sidechest60.png')}/>
             <Title>SideChest'o</Title>
             <FormRow>
                 <Label>E-mail:</Label>
@@ -15,9 +28,17 @@ export default function Login() {
                 <Label>Senha:</Label>
                 <TextInput placeholder="senha"></TextInput>
             </FormRow>
-            <Button>
-                <ButtonText>Login</ButtonText>
-            </Button>
+            <Button
+                onPress={handleComeco}
+                title="Login"
+                color="#fffff"
+             />
+            <Button
+                onPress={handleComeco}
+                title="Cadastrar"
+                color="#fffff"
+            />
         </Container>
     );
 } 
+
